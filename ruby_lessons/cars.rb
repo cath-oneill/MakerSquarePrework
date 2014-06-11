213fb75f7895b45d3deb82ecf04d09fe249f39d6
Catherine O'Neill <cathzozo@gmail.com>
2014-06-11 18:29:47 -0500

    3 new methods in Cars.rb
    1. color= setter
    2. class method most_popular_color
    3. color_into_hash — originally part of initialize, but now a separate
    method that is called by initialize and color=

--- ruby_lessons/cars.rb
+++ ruby_lessons/cars.rb
@@ -13,26 +13,25 @@ class Car
 	def initialize(color)
 		@fuel = 10
 		@distance = 0
-		
+		@color = color
+
 		#increase total_car_count each time a new car is initialized
 		@@total_car_count += 1
 		
 		#allows user to insert string or symbol
-		#if a string is put in for the color, it will be converted to a string
-		if color.is_a? String
-			color_as_symbol = color.to_sym
-		else 
-			color_as_symbol = color
-		end
-		@color = color_as_symbol
+		#if a string is put in for the color, it will be converted to a symbol
+		color_into_hash(color.to_sym)
+	end
 
-		#put info about color into hash
+	def color_into_hash(color_as_symbol)
+			#put info about color into hash
 		if @@cars_per_color.has_key?(color_as_symbol)
 			@@cars_per_color[color_as_symbol] += 1
 		else 
 			@@cars_per_color[color_as_symbol] = 1
 		end
 	end
+
 
 	def to_s()
 		"I'm a #{@color} car! I've driven #{@distance} and have #{@fuel} gallons gas left."
@@ -54,6 +53,19 @@ class Car
 		puts "You must pay $#{3.5 * gallons_needed}"
 		@fuel = 10.0
 	end
+
+	def color=(new_color)
+		@@cars_per_color[@color] -= 1
+		color_into_hash(new_color.to_sym)
+	end
+
+	def self.most_popular_color
+		common_color_pair = @@cars_per_color.max_by {|key, value| value}
+		common_color = common_color_pair[0]
+		puts "The most popular color is #{common_color.to_s}."
+		common_color
+	end
+
 end
 
 car_a = Car.new("blue")
@@ -75,3 +87,13 @@ c2 = Car.new(:blue)
 puts Car.total_car_count
 
 p Car.cars_per_color
+Car.most_popular_color
+c2.color = ("green")
+p Car.cars_per_color
+Car.most_popular_color
+
+best_color = Car.most_popular_color
+c3 = Car.new(best_color)
+puts c3
+p Car.cars_per_color
+
