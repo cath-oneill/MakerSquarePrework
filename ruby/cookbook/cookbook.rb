7d439dd317ec8d6bee98d2206b62732aecc95fdb
Catherine O'Neill <cathzozo@gmail.com>
2014-06-10 23:04:44 -0500

    Cookbook edits
    Added add_recipe method to Cookbook class
    Added new test code file

--- ruby/cookbook/cookbook.rb
+++ ruby/cookbook/cookbook.rb
@@ -2,9 +2,15 @@ class Cookbook
 
 	def initialize(title)
 		@title = title
+		@recipes = []
 	end 
 
 	attr_accessor :title
+	attr_reader :recipes
+
+	def add_recipe(recipe)
+		@recipes << recipe
+	end
 
 end
 #end of Cookbook class
@@ -21,7 +27,6 @@ class Recipe
 	attr_accessor :title
 	attr_accessor :ingredients
 	attr_accessor :steps
-
 
 end
 #end of Recipe class
