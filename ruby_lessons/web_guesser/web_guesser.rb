cafcd77cc02530dd807bb290d0bee5ceb855aa8e
Catherine O'Neill <cathzozo@gmail.com>
2014-07-01 00:23:30 -0500

    number guesser game complete
    —implemented “too high”, “too low”, “WAY too high”. “WAY too low”
    messages
    —user only has 5 chances to guess
    —used set/settings for secret_number and times_guessed

--- ruby_lessons/web_guesser/web_guesser.rb
+++ ruby_lessons/web_guesser/web_guesser.rb
@@ -1,9 +1,44 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-random_number = rand(100) + 1
+set :secret_number, rand(100) + 1
+set :times_guessed, 0
 
 
-get '/'  do
-	erb :index, :locals => {:random_number => random_number}
+def check_guess(number)
+	number = number.to_i
+	
+	if number==0
+		"Enter your best guess! Pick a number between 1 and 100.  You only have 5 turns to guess the correct number."
+	elsif number == settings.secret_number 	
+		settings.secret_number = rand(100) + 1
+		settings.times_guessed = 0
+		"You got it right! Are you ready to play again?  Go ahead and make your next guess!"
+	elsif settings.times_guessed < 4
+		if number > settings.secret_number + 20
+			settings.times_guessed += 1
+			"#{number} is WAY too high! You have guessed #{settings.times_guessed} times."
+		elsif number < settings.secret_number - 20
+			settings.times_guessed += 1
+			"#{number} is WAY too low! You have guessed #{settings.times_guessed} times."
+		elsif number > settings.secret_number
+			settings.times_guessed += 1
+			"#{number} is too high! You have guessed #{settings.times_guessed} times."
+		elsif number < settings.secret_number
+			settings.times_guessed += 1
+			"#{number} is too low! You have guessed #{settings.times_guessed} times."
+		end
+	else			
+		settings.secret_number = rand(100) + 1
+		settings.times_guessed = 0
+		"You lost! You've already guessed 5 times.  A new secret number has been set; try again."
+	end		
 end
+
+
+get '/'  do 
+	user_guess = params["guess"]
+	message = check_guess(user_guess)
+	erb :index, :locals => {:message => message}
+end
+
