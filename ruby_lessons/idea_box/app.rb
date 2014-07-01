767d32b92e6754389a0cdb2e4f49dde85624fa7a
Catherine O'Neill <cathzozo@gmail.com>
2014-07-01 12:17:59 -0500

    yaml set up
    —working at command line

--- ruby_lessons/idea_box/app.rb
+++ ruby_lessons/idea_box/app.rb
@@ -1,14 +1,23 @@
-class IdeaBoxApp < Sinatra::Base
-  not_found do
-    erb :error
-  end
-  
-  configure :development do
-    register Sinatra::Reloader
-  end
+require './idea'
 
-  get '/' do
-    erb :index
-  end
+
+class IdeaBoxApp < Sinatra::Base
+	not_found do
+		erb :error
+	end
+
+	configure :development do
+		register Sinatra::Reloader
+	end
+
+	post '/' do
+		idea = Idea.new
+		idea.save
+		"Creating an idea!"
+	end
+
+	get '/' do
+    	erb :index
+	end
 
 end
